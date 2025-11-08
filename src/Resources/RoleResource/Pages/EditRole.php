36b63d0a4e37af61a725d93ee49d527034dc79bf
amanank <10206965+amanank@users.noreply.github.com>
2025-11-08 15:31:52 +0000

    Fix styling

--- src/Resources/RoleResource/Pages/EditRole.php
+++ src/Resources/RoleResource/Pages/EditRole.php
@@ -9,18 +9,21 @@ use Filament\Resources\Pages\EditRecord;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
 
-class EditRole extends EditRecord {
+class EditRole extends EditRecord
+{
     protected static string $resource = RoleResource::class;
 
     public Collection $permissions;
 
-    protected function getActions(): array {
+    protected function getActions(): array
+    {
         return [
             Actions\DeleteAction::make(),
         ];
     }
 
-    protected function mutateFormDataBeforeSave(array $data): array {
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
         $this->permissions = collect($data)
             ->filter(function ($permission, $key) {
                 return ! in_array($key, ['name', 'guard_name', 'select_all', Utils::getTenantModelForeignKey()]);
@@ -36,7 +39,8 @@ class EditRole extends EditRecord {
         return Arr::only($data, ['name', 'guard_name']);
     }
 
-    protected function afterSave(): void {
+    protected function afterSave(): void
+    {
         $permissionModels = collect();
         $this->permissions->each(function ($permission) use ($permissionModels) {
             $permissionModels->push(Utils::getPermissionModel()::firstOrCreate([
